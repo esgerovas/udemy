c88b8d375b15fbcdd6236973318aed2ebc0cf725
Sara Asgerova <sara.a@code.edu.az>
2016-09-30 17:05:22 +0400

    database

--- udemy_az (1).sql
+++ udemy_az (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 26, 2016 at 07:30 
+-- Generation Time: Sep 30, 2016 at 03:03 
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -41,7 +41,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Sara', 'sara.a@code.edu.az', '$2y$10$KwHXmw3a5HBOyMK4xfbSpei4JUZ/lbpsvVRjACJzOxxigNnyrx1oO', NULL, '2016-09-26 12:07:12', '2016-09-26 12:07:12');
+(1, 'Sara Esgerova', 'sara.a@code.edu.az', '$2y$10$syp6vSoe/wIFO69SZ.jH3enpzrVyM4qR20OvHgwdvgiQt83vHlGn6', NULL, '2016-09-29 23:21:28', '2016-09-29 23:21:28');
 
 -- --------------------------------------------------------
 
@@ -61,8 +61,7 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `slug`, `icon`) VALUES
-(1, 'Development', 'Development', 'fa fa-phone'),
-(3, 'Business', 'Business', 'fa fa-envelope');
+(1, 'Development', 'development', 'ud_development');
 
 -- --------------------------------------------------------
 
@@ -73,8 +72,8 @@ INSERT INTO `categories` (`id`, `name`, `slug`, `icon`) VALUES
 CREATE TABLE `courses` (
   `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `headline` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `headline` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
   `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0',
@@ -85,6 +84,13 @@ CREATE TABLE `courses` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `title`, `headline`, `description`, `slug`, `image`, `status`, `teacher_id`, `subcategory_id`, `level_id`, `language_id`, `created_at`, `updated_at`) VALUES
+(2, 'Complete PHP MVC tutorial. Create your own PHP MVC framework', 'Create your own PHP MVC framework and CMS step by step. Learn in which way popular PHP frameworks work.', '<p><strong>Learn MVC by creating your own PHP framework step by step!</strong></p>\r\n\r\n<p>MVC pattern, which stands for Model View Controller, is a widely &ndash; used programming technology and the most popular technique in web applications development.</p>\r\n\r\n<p>The most popular frameworks such as Laravel, Symphony, Yii, Zend are based on MVC pattern. So, it undoubtedly worth learning.</p>\r\n\r\n<p><strong>The best way of learning it is creating your own MVC framework and CMS.</strong></p>\r\n\r\n<p>Step by step we`ll be creating MVC web application. Further the result of application developing during this course may be put into practice.</p>\r\n\r\n<p>As a consequence, we will create a light-weight framework and CMS which may be used in your project. It would contain pages view and editing, contact form, admin section and authorization.</p>\r\n\r\n<p>During this course you will learn the best practices in PHP OOP, sessions and MySQL interaction.</p>\r\n\r\n<p>If you always wanted to know how modern web applications and frameworks work, this course is definitely for you!</p>\r\n\r\n<p>Basic knowledge of PHP OOP is all that you need.</p>\r\n\r\n<p>Feel free to ask about free web hosting for this course learning. You can obtain domain, ftp and mysql accounts absolutely free in order to work with this course.</p>\r\n\r\n<p>On completing this course you will be a PHP-Academy certified developer.</p>\r\n\r\n<h3>What are the requirements?</h3>\r\n\r\n<ul>\r\n	<li>You should already be familiar with PHP basics</li>\r\n	<li>You should have web server and MySQL installed on your system or hosting. I you haven&#39;t - ask us for free hosting for this course students</li>\r\n	<li>You should have IDE installed. We recommend PHP Storm but Notepad++ is also affordable</li>\r\n</ul>\r\n\r\n<h3>What am I going to get from this course?</h3>\r\n\r\n<ul>\r\n	<li>Knowing about how MVC framework works</li>\r\n	<li>You will create your own MVC framework</li>\r\n	<li>You will also create a simple CMS based on that framework</li>\r\n	<li>After completing this course it would be easier to you to learn popular MVC frameworks such as Laravel, Symfony, Yii, Zend etc</li>\r\n</ul>\r\n\r\n<h3>What is the target audience?</h3>\r\n\r\n<ul>\r\n	<li>This course is created for those who are familiar with PHP basics and would like to learn how modern web projects work</li>\r\n</ul>\r\n', 'complete-php-mvc-tutorial-create-your-own-php-mvc-framework', '4127.jpg', 1, 1, 2, 1, 1, '2016-09-30 00:30:44', '2016-09-30 00:30:44');
 
 -- --------------------------------------------------------
 
@@ -102,10 +108,7 @@ CREATE TABLE `languages` (
 --
 
 INSERT INTO `languages` (`id`, `name`) VALUES
-(1, 'Azərbaycanca'),
-(4, 'İngiliscə'),
-(3, 'Rusca'),
-(2, 'Türkcə');
+(1, 'Ingilis dili');
 
 -- --------------------------------------------------------
 
@@ -115,14 +118,25 @@ INSERT INTO `languages` (`id`, `name`) VALUES
 
 CREATE TABLE `lectures` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
-  `slug` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
-  `text` text COLLATE utf8_unicode_ci NOT NULL,
-  `order` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `video_link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `youtube_link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `duration` time NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `text` text COLLATE utf8_unicode_ci,
+  `order` int(11) NOT NULL,
   `section_id` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `lectures`
+--
+
+INSERT INTO `lectures` (`id`, `name`, `video_link`, `youtube_link`, `duration`, `slug`, `text`, `order`, `section_id`, `created_at`, `updated_at`) VALUES
+(1, 'Introduction to MVC', '6555.jpg', '', '00:00:00', 'introduction-to-mvc', '', 1, 2, '2016-09-30 00:38:54', '2016-09-30 00:38:54'),
+(3, 'xdfsd', '7958.jpg', '', '00:00:00', 'xdfsd', 'dfgd', 1, 2, '2016-09-30 08:40:12', '2016-09-30 08:40:12');
 
 -- --------------------------------------------------------
 
@@ -140,9 +154,7 @@ CREATE TABLE `levels` (
 --
 
 INSERT INTO `levels` (`id`, `name`) VALUES
-(1, 'Başlanğıc səviyə'),
-(2, 'Orta səviyə'),
-(3, 'Üst səviyə');
+(1, 'Bütün səviyə');
 
 -- --------------------------------------------------------
 
@@ -218,6 +230,13 @@ CREATE TABLE `sections` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `sections`
+--
+
+INSERT INTO `sections` (`id`, `name`, `order`, `course_id`, `created_at`, `updated_at`) VALUES
+(2, ' Introduction', 1, 2, '2016-09-30 00:31:09', '2016-09-30 00:31:09');
+
 -- --------------------------------------------------------
 
 --
@@ -237,10 +256,8 @@ CREATE TABLE `subcategories` (
 --
 
 INSERT INTO `subcategories` (`id`, `name`, `slug`, `icon`, `category_id`) VALUES
-(5, 'Web Develop', 'Web Development', 'fa fa-arrows', 1),
-(6, 'Web', 'Web', 'fa fa-star', 3),
-(7, 'Excell', 'Excell', 'fa fa-facebook', 3),
-(8, 'Word', 'Word', 'fa fa-twitter', 3);
+(1, 'Web Development', 'web-development', 'ud_web-development', 1),
+(2, 'Programming Language', 'programming-language', 'ud_programming-languages', 1);
 
 -- --------------------------------------------------------
 
@@ -250,23 +267,30 @@ INSERT INTO `subcategories` (`id`, `name`, `slug`, `icon`, `category_id`) VALUES
 
 CREATE TABLE `teachers` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `surname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `job` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `biography` text COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `facebook_link` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `linked_link` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `twitter_link` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `youtube_link` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `website_link` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `number` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `age` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `job` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `biography` text COLLATE utf8_unicode_ci,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `facebook_link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `linked_link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `twitter_link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `youtube_link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `website_link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `number` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`id`, `name`, `job`, `biography`, `email`, `facebook_link`, `linked_link`, `twitter_link`, `youtube_link`, `website_link`, `image`, `password`, `number`, `age`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'PHP Academy', 'Web development courses', 'PHP Academy is surely one of the most effective ways for you to acquire all the necessary skills in web development since we truly wish all of our students to become really talented professionals.\r\n\r\nWe have a great experience in teaching which means that we already have our own magic formula of providing foundation knowledge, so that you will be able to create your own web sites, get the desired job and well-paid position. What is more, we constantly improve ourstrategy of teaching to make our students feel more comfortable, confident and satisfied. Our aim is to deliver valuable knowledge, provide you with pretty simple, succinct, comprehensible and qualitative materials at quite acceptable price. We are absolutely open to our students that`s why you are welcome to ask any questions which we are really thankful for.\r\n\r\nYou should also notice, that all of our courses have money back guarantee, which lasts 30 days.\r\n\r\nWe wish you happy learning and successful digesting of our materials!', 'sara.a@code.edu.az', NULL, NULL, NULL, NULL, NULL, 'default.png', '123456', NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -276,16 +300,15 @@ CREATE TABLE `teachers` (
 
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `surname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'default.jpg',
-  `last_login_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `status` tinyint(1) NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'default.png',
+  `last_login_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -298,7 +321,7 @@ CREATE TABLE `watchings` (
   `id` int(10) UNSIGNED NOT NULL,
   `course_id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
-  `rating` int(10) UNSIGNED NOT NULL,
+  `rating` int(11) DEFAULT NULL,
   `feedback` text COLLATE utf8_unicode_ci,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -391,8 +414,7 @@ ALTER TABLE `subcategories`
 --
 ALTER TABLE `teachers`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `teachers_email_unique` (`email`),
-  ADD UNIQUE KEY `teachers_number_unique` (`number`);
+  ADD UNIQUE KEY `teachers_email_unique` (`email`);
 
 --
 -- Indexes for table `users`
@@ -422,27 +444,27 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `languages`
 --
 ALTER TABLE `languages`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `lectures`
 --
 ALTER TABLE `lectures`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `levels`
 --
 ALTER TABLE `levels`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `questions`
 --
@@ -452,17 +474,17 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT for table `sections`
 --
 ALTER TABLE `sections`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `subcategories`
 --
 ALTER TABLE `subcategories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
